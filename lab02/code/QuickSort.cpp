b985bef205a58debe3608238ed7b65bf4579af81
root <root@precise64.(none)>
2017-09-10 19:43:26 +0000

    new changes

--- lab02/code/QuickSort.cpp
+++ lab02/code/QuickSort.cpp
@@ -1,76 +1,52 @@
-
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-
+# include <iostream>
+# include <cstdlib>
+# include <ctime>
+# include <string>
 using namespace std;
-
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-   int partition (int * arr, int low, int high)
-   {
-       int pivot = arr[high];    // pivot
-       int i = (low - 1);  // Index of smaller element
-    
-       for (int j = low; j <= high- 1; j++)
-       {
-           // If current element is smaller than or
-           // equal to pivot
-           if (arr[j] <= pivot)
-           {
-               i++;    // increment index of smaller element
-               swap(&arr[i], &arr[j]);
-           }
-       }
-       swap(&arr[i + 1], &arr[high]);
-       return (i + 1);
-   }
-    
-   /* The main function that implements QuickSort
-    arr[] --> Array to be sorted,
-     low  --> Starting index,
-     high  --> Ending index */
-   void MyFunc(int *array, int low, int high)
-   {
-       if (low < high)
-       {
-           /* pi is partitioning index, arr[p] is now
-              at right place */
-           int pi = partition(array, low, high);
-    
-           // Separately sort elements before
-           // partition and after partition
-           MyFunc(array, low, pi - 1);
-           MyFunc(array, pi + 1, high);
-       }
-   }
-
-int main(int argc,char **argv) {
-
-  int *Sequence;
-  int arraySize;
-  // Get the size of the sequence
-  cin >> arraySize;
-
-  // Allocate enough memory to store "arraySize" integers
-  Sequence = new int[arraySize];
-    
-  // Read in the sequence
-  for ( int i=0; i<arraySize; i++ ) {
-    cin >> Sequence[i];
+int quickSort(int *array, int p, int r)
+{ 
+  int pivot = array[r];
+  int i = (p - 1); 
+  for (int c = p; c <= (r - 1); c++)
+  {
+    if (array[c] <= pivot)
+    {
+      i++;
+      swap(&array[i], &array[c]);
+    }
   }
-  // Run your algorithms to manipulate the elements in Sequence
-  MyFunc( Sequence, 0, arraySize - 1); 
-    
-  // Output the result
-  for(int i=0; i<arraySize; i++)
-      cout << Sequence[i] << endl;
-    
-  // Free allocated space
+  swap(&array[i + 1], &array[r]);
+  return (i + 1);
+}
+
+void sort(int *array, int p, int r)
+{
+  if (p < r)
+  {
+    int pivot = array[(rand() % (r + 1))]; 
+    sort(array, p, (pivot - 1));
+    sort(array, (pivot + 1), r);
+  }
+  return;
+}
+
+int main(int argc, char **argv)
+{
+  int arraySize, *Sequence;
+  cin >> arraySize;
+  Sequence = new int[arraySize];
+  for (int c = 0; c < arraySize; c++)
+    cin >> Sequence[c];
+  sort(Sequence, 0, (arraySize - 1)); 
+  for(int c = 0; c < arraySize; c++)
+      cout << Sequence[c] << endl;
   delete[] Sequence;
+  return 0;
 }
